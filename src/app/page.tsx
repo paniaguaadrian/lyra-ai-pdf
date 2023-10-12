812640bfbc1c2bdfa852817591a3ea8ed6c2b644
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2023-10-12 12:30:54 +0200

    feat(configuration): add shadcn/ui and link to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import MaxWidhtWrapper from '@/components/MaxWidhtWrapper';
 
 export default function Home() {
     return (
-        <MaxWidhtWrapper className="bg-red-500">
-            <div>Hi!</div>
+        <MaxWidhtWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+            <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+                <p className="text-sm font-semibold text-gray-700">
+                    Lyra is now public!
+                </p>
+            </div>
         </MaxWidhtWrapper>
     );
 }
