5ec708ae25a9824f7d7a8ba3ec688c97290e7f30
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2023-10-12 11:55:38 +0200

    feat(configuration): add maxWidthWrapper and cn for combining styles

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidhtWrapper from '@/components/MaxWidhtWrapper';
+
+export default function Home() {
+    return (
+        <MaxWidhtWrapper className="bg-red-500">
+            <div>Hi!</div>
+        </MaxWidhtWrapper>
+    );
+}
